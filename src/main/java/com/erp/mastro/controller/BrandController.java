4d4a5e688f7523e93cf98154ebbed5e6e35fb879
Anju Jose <anjujose995@gmail.com>
2020-06-30 11:56:18 +0530

    Edit brand

--- src/main/java/com/erp/mastro/controller/BrandController.java
+++ src/main/java/com/erp/mastro/controller/BrandController.java
@@ -1,14 +1,13 @@
 package com.erp.mastro.controller;
 
+import com.erp.mastro.custom.responseBody.GenericResponse;
 import com.erp.mastro.entities.Brand;
 import com.erp.mastro.model.request.BrandRequestModel;
 import com.erp.mastro.service.interfaces.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -48,5 +47,24 @@ public class BrandController {
         }
     }
 
+    @GetMapping("/master/getBrandForEdit")
+    @ResponseBody
+    public GenericResponse getBrandForEdit(Model model, HttpServletRequest request, @RequestParam("brandId") Long brandId) {
+
+        try {
+
+            Brand brandDetails = brandService.getBrandId(brandId);
+            return new GenericResponse(true, "get brand details")
+                    .setProperty("brandId", brandDetails.getId())
+                    .setProperty("brandName", brandDetails.getBrandName())
+                    .setProperty("brandDescription", brandDetails.getBrandDescription());
+
+        } catch (Exception e) {
+
+            return new GenericResponse(false, e.getMessage());
+
+        }
+
+    }
 
 }
--- src/main/resources/static/resources/js/mastro/master.js
+++ src/main/resources/static/resources/js/mastro/master.js
@@ -15,6 +15,41 @@ $(document).ready(function(){
 
 });
 
+ //get brand edit start
+  $("body").on('click','.brandEdit',function (e) {
+
+    e.preventDefault();
+     var brandId=$(this).data('brandid');
+     alert(brandId);
+     	$.ajax({
+      url: '/master/getBrandForEdit',
+      type: 'GET',
+      dataType : 'json',
+      data: { 'brandId': brandId },
+
+      success: function(data){
+               if(data.success) {
+                           alert("ok");
+                           alert(data.data.brandId);
+                           alert(data.data.brandName);
+                           alert(data.data.brandDescription);
+
+                            $('#brandId').val(data.data.brandId);
+                           $('#brandnames').val(data.data.brandName);
+
+                           $('#branddescription').val(data.data.brandDescription);
+
+                                }
+
+                           },
+
+  error: function(jqXHR, textStatus)
+   {
+   alert('Error Occured');
+    }
+       });
+   });
+    //get brand edit end
 
 
 
