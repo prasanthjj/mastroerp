0f9cccc8950ad07d2880b8bc365018ce25b7d846
Prasanth <prashanth@halotechnologysolutions.onmicrosoft.com>
2020-07-01 18:39:36 +0530

    Adding Exception Framework
    Signed-off-by: Prasanth <prashanth@halotechnologysolutions.onmicrosoft.com>

---
+++ src/main/java/com/erp/mastro/exception/MastroServiceException.java
@@ -0,0 +1,11 @@
+package com.erp.mastro.exception;
+
+public class MastroServiceException extends Exception {
+
+    private static final long serialVersionUID = 4227171665478076830L;
+
+
+    public MastroServiceException(String message) {
+        super(message);
+    }
+}
