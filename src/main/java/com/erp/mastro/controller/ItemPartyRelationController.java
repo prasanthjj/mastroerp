0e358a3e0e67288e8fb607f4a11dd4bafececbe9
Anju Jose <anjujose995@gmail.com>
2020-08-18 11:20:13 +0530

    autopopulate methods in new controller

--- src/main/java/com/erp/mastro/controller/ItemPartyRelationController.java
+++ src/main/java/com/erp/mastro/controller/ItemPartyRelationController.java
@@ -1,24 +1,16 @@
 package com.erp.mastro.controller;
 
 import com.erp.mastro.common.MastroLogUtils;
-import com.erp.mastro.custom.responseBody.GenericResponse;
 import com.erp.mastro.dao.AutoPopulateDAO;
-import com.erp.mastro.entities.Party;
 import com.erp.mastro.entities.Product;
-import com.erp.mastro.model.request.PartyRequestModel;
-import com.erp.mastro.model.request.ProductRequestModel;
 import com.erp.mastro.service.interfaces.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Controller to include itemparty methods
@@ -33,6 +25,12 @@ public class ItemPartyRelationController {
     @Autowired
     ProductService productService;
 
+    /**
+     * Method to get itemparty page
+     *
+     * @param model
+     * @return
+     */
     @GetMapping("/getItemPartys")
     public String getItemPartys(Model model) {
         MastroLogUtils.info(ItemPartyRelationController.class, "Going to get ItemParty : {}");
@@ -51,77 +49,10 @@ public class ItemPartyRelationController {
     }
 
     /**
-     * Method to get autopopulate items
-     *
-     * @param searchTerm
-     * @return item list
-     */
-    @RequestMapping(value = "/autopopulate/items", method = RequestMethod.GET)
-    @ResponseBody
-    public GenericResponse products(@RequestParam("searchTerm") String searchTerm) {
-        try {
-            MastroLogUtils.info(ItemPartyRelationController.class, "Going to get product in autocomplete : {}");
-            List<Product> productsFinal = new ArrayList<>();
-            List<Product> products = autoPopulateDao.getAutoPopulateList("productName", searchTerm, Product.class, 50);
-            productsFinal = products.stream()
-                    .filter(productData -> (null != productData))
-                    .filter(productData -> (true == productData.isEnabled()))
-                    .collect(Collectors.toList());
-            Set<ProductRequestModel> productRequestModels = new HashSet<ProductRequestModel>();
-            for (Product product : productsFinal) {
-                ProductRequestModel productRequestModel = new ProductRequestModel();
-                productRequestModel.setId(product.getId());
-                productRequestModel.setProductname(product.getProductName());
-                productRequestModels.add(productRequestModel);
-            }
-            return new GenericResponse(true, "get items")
-                    .setProperty("products", productRequestModels);
-
-        } catch (Exception e) {
-            MastroLogUtils.error(this, e.getMessage(), e);
-            throw e;
-        }
-    }
-
-    /**
-     * Get parts in autocomplete
-     *
-     * @param searchTerm
-     * @return partys
-     */
-    @RequestMapping(value = "/autopopulate/party", method = RequestMethod.GET)
-    @ResponseBody
-    public GenericResponse getPartys(@RequestParam("searchTerm") String searchTerm) {
-        try {
-            MastroLogUtils.info(ItemPartyRelationController.class, "Going to get party in autocomplete : {}");
-            List<Party> partyFinal = new ArrayList<>();
-            List<Party> partys = autoPopulateDao.getAutoPopulateList("partyName", searchTerm, Party.class, 50);
-            partyFinal = partys.stream()
-                    .filter(partysData -> (null != partysData))
-                    .filter(partysData -> (true == partysData.isEnabled()))
-                    .collect(Collectors.toList());
-            Set<PartyRequestModel> partyRequestModels = new HashSet<PartyRequestModel>();
-            for (Party party : partyFinal) {
-                PartyRequestModel partyRequestModel = new PartyRequestModel();
-                partyRequestModel.setId(party.getId());
-                partyRequestModel.setPartysname(party.getPartyName());
-                partyRequestModels.add(partyRequestModel);
-            }
-            return new GenericResponse(true, "get partys")
-                    .setProperty("partys", partyRequestModels);
-
-        } catch (Exception e) {
-            MastroLogUtils.error(this, e.getMessage(), e);
-            throw e;
-        }
-    }
-
-    /**
-     * Method to get product details
-     *
+     * Method to get selected product details
      * @param model
      * @param req
-     * @return itemparty page
+     * @return selected product details
      */
     @GetMapping("/getSelectedProduct")
     public String getSelectedProduct(Model model, HttpServletRequest req) {
