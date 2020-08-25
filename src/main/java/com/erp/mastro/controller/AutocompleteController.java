76efd9a70b65db7f0c2a273d20e1b9b8ca0654b3
Anju Jose <anjujose995@gmail.com>
2020-08-25 15:52:34 +0530

    autopoulate in indent item

--- src/main/java/com/erp/mastro/controller/AutocompleteController.java
+++ src/main/java/com/erp/mastro/controller/AutocompleteController.java
@@ -3,10 +3,13 @@ package com.erp.mastro.controller;
 import com.erp.mastro.common.MastroLogUtils;
 import com.erp.mastro.custom.responseBody.GenericResponse;
 import com.erp.mastro.dao.AutoPopulateDAO;
+import com.erp.mastro.entities.Branch;
 import com.erp.mastro.entities.Party;
 import com.erp.mastro.entities.Product;
+import com.erp.mastro.entities.Stock;
 import com.erp.mastro.model.request.PartyRequestModel;
 import com.erp.mastro.model.request.ProductRequestModel;
+import com.erp.mastro.service.IndentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,12 @@ public class AutocompleteController {
 
     @Autowired
     AutoPopulateDAO autoPopulateDao;
+
+    @Autowired
+    IndentServiceImpl indentService;
+
+    @Autowired
+    UserController userController;
 
     /**
      * Method to get autopopulate items
@@ -124,6 +133,56 @@ public class AutocompleteController {
             }
             return new GenericResponse(true, "get partys")
                     .setProperty("partys", partyRequestModels);
+
+        } catch (Exception e) {
+            MastroLogUtils.error(this, e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    /**
+     * autocomplete method for item in indent shows only that branch stock items
+     *
+     * @param searchTerm
+     * @return the item list
+     */
+    @RequestMapping(value = "/items/indent", method = RequestMethod.GET)
+    @ResponseBody
+    public GenericResponse productsInIndent(@RequestParam("searchTerm") String searchTerm) {
+        try {
+            MastroLogUtils.info(AutocompleteController.class, "Going to get product in autocomplete : {}");
+            List<Product> productsFinal = new ArrayList<>();
+            List<Product> products = autoPopulateDao.getAutoPopulateList("productName", searchTerm, Product.class, 50);
+            productsFinal = products.stream()
+                    .filter(productData -> (null != productData))
+                    .filter(productData -> (true == productData.isEnabled()))
+                    .collect(Collectors.toList());
+
+            Branch currentBranch = userController.getCurrentUser().getUserSelectedBranch().getCurrentBranch();
+            Set<Product> productSet = new HashSet<>();
+            for (Product product : productsFinal) {
+                Set<Stock> stocksSet = indentService.getAllStocks().stream()
+                        .filter(stockData -> (null != stockData))
+                        .filter(stockData -> (currentBranch.getId() == stockData.getBranch().getId()))
+                        .filter(stockData -> (product.getId() == stockData.getProduct().getId()))
+                        .filter(stockData -> (1 != stockData.getStockDeleteStatus()))
+                        .collect(Collectors.toSet());
+                if (stocksSet.isEmpty() == false) {
+                    Stock stock = stocksSet.stream().findFirst().get();
+                    productSet.add(stock.getProduct());
+                } else {
+                    MastroLogUtils.info(AutocompleteController.class, "stock set is empty for the product" + product.getId() + "in branch" + currentBranch.getId());
+                }
+            }
+            Set<ProductRequestModel> productRequestModels = new HashSet<ProductRequestModel>();
+            for (Product product : productSet) {
+                ProductRequestModel productRequestModel = new ProductRequestModel();
+                productRequestModel.setId(product.getId());
+                productRequestModel.setProductname(product.getProductName());
+                productRequestModels.add(productRequestModel);
+            }
+            return new GenericResponse(true, "get items")
+                    .setProperty("products", productRequestModels);
 
         } catch (Exception e) {
             MastroLogUtils.error(this, e.getMessage(), e);
